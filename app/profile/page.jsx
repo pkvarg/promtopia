e7f22de8ac541a015423fcb4fd5916c3578bfbbc
peter varga <pkvarg@yahoo.se>
2023-05-11 20:58:38 +0200

    2:48

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,8 +1,34 @@
 'use client'
 import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import Profile from '@components/Profile'
 
-const Profile = () => {
-  return <div> Profile </div>
+const MyProfile = () => {
+  const { data: session } = useSession()
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`)
+      const data = await response.json()
+      setPosts(data)
+    }
+    if (session?.user.id) fetchPost()
+  }, [])
+
+  const handleEdit = () => {}
+  const handleDelete = async () => {}
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page'
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  )
 }
 
-export default Profile
+export default MyProfile
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -3,8 +3,13 @@ import { useState } from 'react'
 import Image from 'next/image'
 import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
+import { userAgent } from 'next/server'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
+
   const [copied, setCopied] = useState('')
   const handleCopy = () => {
     setCopied(post.prompt)
@@ -44,6 +49,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 ? '/assets/icons/tick.svg'
                 : '/assets/icons/copy.svg'
             }
+            alt='copy_icon'
             width={12}
             height={12}
           />
@@ -56,6 +62,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
