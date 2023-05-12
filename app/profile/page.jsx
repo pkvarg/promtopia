523aaec306302a9419577dade9efa7e0164398b5
peter varga <pkvarg@yahoo.se>
2023-05-12 16:45:09 +0200

    03:06

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation'
 import Profile from '@components/Profile'
 
 const MyProfile = () => {
+  const router = useRouter()
+
   const { data: session } = useSession()
 
   const [posts, setPosts] = useState([])
@@ -18,8 +20,24 @@ const MyProfile = () => {
     if (session?.user.id) fetchPost()
   }, [])
 
-  const handleEdit = () => {}
-  const handleDelete = async () => {}
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  }
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt?')
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        })
+        const filteredPosts = posts.filter((p) => p._id !== post._id)
+        setPosts(filteredPosts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
   return (
     <Profile
       name='My'
